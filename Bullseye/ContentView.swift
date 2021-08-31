f28ac032e8a4269d9dceb63b824cb9b4fef88c41
anastasiia <a.ananas11@yandex.ru>
2021-08-31 10:41:25 +0300

    off Portrait Orientation, add some comments

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	// Properties
+	// ==========
+	
+	// User interface views
 	@State var alertIsVisible: Bool = false
 	
+	// User interface content and layout
     var body: some View {
 		VStack {
+			// Target row
 			Text("Welcome to my first app!")
 				.fontWeight(.black)
 				.foregroundColor(.green)
 				.padding()
+			
+			// Slider row
+			// TODO: Add views for the slider row here.
+			
+			// Button row
 			Button(action: {
 				print("Button pressed!")
 				self.alertIsVisible = true
@@ -27,12 +39,20 @@ struct ContentView: View {
 					  message: Text("This is my first pop-up!"), dismissButton:
 					.default(Text("Awesome!")))
 			})
+			
+			// Score row
+			// TODO: Add views for the score, rounds, and start and info buttons here.
 		}
     }
+	// Methods
+	// =======
 }
 
+// Preview
+// =======
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+} #endif
