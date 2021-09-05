ab1b5c9d39793b40a03706713dde587094be1cca
anastasiia <a.ananas11@yandex.ru>
2021-09-05 22:37:56 +0300

    generating a random target number

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
 	// User interface views
 	@State var alertIsVisible: Bool = false
 	@State var sliderValue: Double = 50.0
+	@State var target: Int = Int.random(in: 1...100)
 	
 	// User interface content and layout
     var body: some View {
@@ -24,7 +25,7 @@ struct ContentView: View {
 			// Target row
 			HStack {
 				Text("Put the bullseye as close as you can to:")
-				Text("100")
+				Text("\(self.target)")
 			}
 			
 			Spacer()
